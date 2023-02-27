d165e23f86ad51f052e98bbb6db4baec1937188e
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-02-27 21:27:57 +0100

    Authored by relationship

--- src/main/java/org/example/PartA2_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartA2_ZivkovicIvanovic.java
@@ -17,6 +17,8 @@ import static org.neo4j.driver.Values.parameters;
 public class PartA2_ZivkovicIvanovic implements AutoCloseable {
     private final Driver driver;
 
+    private static final int ENTITY_LIMIT = 10000;
+
     public PartA2_ZivkovicIvanovic(String uri, String user, String password) {
         this.driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
     }
@@ -26,41 +28,78 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
         driver.close();
     }
 
+    public void createConstraints() {
+        System.out.println("Creating constraints...");
+        try (var session = driver.session()) {
+            session.executeWriteWithoutResult(tx -> {
+                tx.run("""
+                        CREATE CONSTRAINT authorIdConstraint IF NOT EXISTS
+                        FOR (author:Author)
+                        REQUIRE author.ID IS UNIQUE
+                        """);
+            });
+            session.executeWriteWithoutResult(tx -> {
+                tx.run("""
+                        CREATE CONSTRAINT articleIdConstraint IF NOT EXISTS
+                        FOR (article:Article)
+                        REQUIRE article.ID IS UNIQUE
+                        """);
+            });
+        }
+    }
+
     public void loadAuthors() {
         System.out.println("Loading authors...");
         try (var session = driver.session()) {
-            session.run("""
-                    LOAD CSV WITH HEADERS FROM 'file:///output_author.csv' AS line FIELDTERMINATOR ';'
+            session.run(String.format("""
+                    LOAD CSV FROM 'file:///output_author.csv' AS line FIELDTERMINATOR ';'
+                    WITH line LIMIT %d
                     CALL {
                         WITH line
-                        CREATE (:Author {ID: toInteger(line.ID), author: line.author})
+                        CREATE (:Author {ID: toInteger(line[0]), author: line[1]})
                     }
                     IN TRANSACTIONS
-                    """);
+                    """, ENTITY_LIMIT));
         }
-        System.out.println("Authors loaded.");
     }
 
     public void loadArticles() {
         System.out.println("Loading articles...");
         try (var session = driver.session()) {
-            session.run("""
+            session.run(String.format("""
                     LOAD CSV WITH HEADERS FROM 'file:///output_article.csv' AS line FIELDTERMINATOR ';'
+                    WITH line LIMIT %d
                     CALL {
                         WITH line
-                        CREATE (:Article {ID: toInteger(line.ID), mdate: date(line.mdate), key: line.key, publtype: line.publtype,
+                        CREATE (:Article {ID: toInteger(line.id), mdate: date(line.mdate), key: line.key, publtype: line.publtype,
                             title: line.title, volume: line.volume, month: line.month, year: toInteger(line.year)})
+                    }
+                    IN TRANSACTIONS
+                    """, ENTITY_LIMIT));
+        }
+    }
+
+    public void loadAuthoredBy() {
+        System.out.println("Relating articles with authors...");
+        try (var session = driver.session()) {
+            session.run("""
+                    LOAD CSV FROM 'file:///output_author_authored_by.csv' AS line FIELDTERMINATOR ';'
+                    CALL {
+                        WITH line
+                        MATCH (article:Article {ID: toInteger(line[0])}), (author:Author {ID: toInteger(line[1])})
+                        CREATE (article)-[:Authored_by]->(author)
                     }
                     IN TRANSACTIONS
                     """);
         }
-        System.out.println("Articles loaded.");
     }
 
     public static void main(String... args) {
         try (var loader = new PartA2_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
+            loader.createConstraints();
             loader.loadAuthors();
             loader.loadArticles();
+            loader.loadAuthoredBy();
         }
     }
 }
