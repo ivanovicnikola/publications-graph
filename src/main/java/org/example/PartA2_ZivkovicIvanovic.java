f5fa893e03f1de54db68e9a2c79c0900258fced6
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-02-27 19:16:55 +0100

    Load articles

--- src/main/java/org/example/PartA2_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartA2_ZivkovicIvanovic.java
@@ -5,6 +5,13 @@ import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
 import org.neo4j.driver.Query;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.neo4j.driver.Values.parameters;
 
 public class PartA2_ZivkovicIvanovic implements AutoCloseable {
@@ -20,21 +27,40 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
     }
 
     public void loadAuthors() {
+        System.out.println("Loading authors...");
         try (var session = driver.session()) {
             session.run("""
                     LOAD CSV WITH HEADERS FROM 'file:///output_author.csv' AS line FIELDTERMINATOR ';'
                     CALL {
                         WITH line
-                        CREATE (:Author {ID: line.ID, author: line.author})
+                        CREATE (:Author {ID: toInteger(line.ID), author: line.author})
                     }
                     IN TRANSACTIONS
                     """);
         }
+        System.out.println("Authors loaded.");
+    }
+
+    public void loadArticles() {
+        System.out.println("Loading articles...");
+        try (var session = driver.session()) {
+            session.run("""
+                    LOAD CSV WITH HEADERS FROM 'file:///output_article.csv' AS line FIELDTERMINATOR ';'
+                    CALL {
+                        WITH line
+                        CREATE (:Article {ID: toInteger(line.ID), mdate: date(line.mdate), key: line.key, publtype: line.publtype,
+                            title: line.title, volume: line.volume, month: line.month, year: toInteger(line.year)})
+                    }
+                    IN TRANSACTIONS
+                    """);
+        }
+        System.out.println("Articles loaded.");
     }
 
     public static void main(String... args) {
         try (var loader = new PartA2_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
             loader.loadAuthors();
+            loader.loadArticles();
         }
     }
 }
