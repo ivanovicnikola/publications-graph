c8e267f42528aa1c60360e7ac9ba517ca8c3ee44
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-03-04 17:28:18 +0100

    Load authors changes

--- src/main/java/org/example/PartA2_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartA2_ZivkovicIvanovic.java
@@ -7,8 +7,8 @@ import org.neo4j.driver.GraphDatabase;
 public class PartA2_ZivkovicIvanovic implements AutoCloseable {
     private final Driver driver;
 
-    private static final int ENTITY_LIMIT = 10000;
-    private static final int PROCEEDING_LIMIT = 1000;
+    private static final int ENTITY_LIMIT = 1000;
+    private static final int PROCEEDING_LIMIT = ENTITY_LIMIT/10;
 
     public PartA2_ZivkovicIvanovic(String uri, String user, String password) {
         this.driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
@@ -22,13 +22,6 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
     public void createConstraints() {
         System.out.println("Creating constraints...");
         try (var session = driver.session()) {
-            session.executeWriteWithoutResult(tx -> {
-                tx.run("""
-                        CREATE CONSTRAINT authorIdConstraint IF NOT EXISTS
-                        FOR (author:Author)
-                        REQUIRE author.ID IS UNIQUE
-                        """);
-            });
             session.executeWriteWithoutResult(tx -> {
                 tx.run("""
                         CREATE CONSTRAINT articleIdConstraint IF NOT EXISTS
@@ -57,53 +50,28 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
         }
     }
 
-    public void loadAuthors() {
-        System.out.println("Loading authors...");
-        try (var session = driver.session()) {
-            session.run(String.format("""
-                    LOAD CSV FROM 'file:///output_author.csv' AS line FIELDTERMINATOR ';'
-                    WITH line LIMIT %d
-                    CALL {
-                        WITH line
-                        CREATE (:Author {ID: toInteger(line[0]), author: line[1]})
-                    }
-                    IN TRANSACTIONS
-                    """, ENTITY_LIMIT));
-        }
-    }
-
     public void loadArticles() {
         System.out.println("Loading articles...");
         try (var session = driver.session()) {
             session.run(String.format("""
                     LOAD CSV WITH HEADERS FROM 'file:///output_article.csv' AS line FIELDTERMINATOR ';'
                     WITH line LIMIT %d
-                    WHERE NOT line.volume IS NULL
                     CALL {
                         WITH line
                         MERGE (j:Journal {journal: line.journal})
-                        MERGE (v:Volume {volume: line.volume})-[:PART_OF]->(j)
                         CREATE (a:Article {ID: toInteger(line.id), mdate: date(line.mdate), key: line.key, publtype: line.publtype,
                             title: line.title, month: line.month, year: toInteger(line.year)})
-                        CREATE (a)-[:PUBLISHED_IN]->(v)
+                        CREATE (a)-[:PUBLISHED_IN {volume: line.volume}]->(j)
+                        WITH line, a
+                        UNWIND split(line.author, '|') AS authors
+                        MERGE (author:Author {author: authors})
+                        CREATE (a)-[:AUTHORED_BY]->(author)
+                        WITH author, a, line
+                        WHERE author.author = split(line.author, '|')[0]
+                        CREATE (a)-[:CORRESPONDING_AUTHOR]->(author)
                     }
                     IN TRANSACTIONS
                     """, ENTITY_LIMIT));
-        }
-    }
-
-    public void loadAuthoredBy() {
-        System.out.println("Relating papers with authors...");
-        try (var session = driver.session()) {
-            session.run("""
-                    LOAD CSV FROM 'file:///output_author_authored_by.csv' AS line FIELDTERMINATOR ';'
-                    CALL {
-                        WITH line
-                        MATCH (paper:Article|Inproceeding {ID: toInteger(line[0])}), (author:Author {ID: toInteger(line[1])})
-                        CREATE (paper)-[:AUTHORED_BY]->(author)
-                    }
-                    IN TRANSACTIONS
-                    """);
         }
     }
 
@@ -133,6 +101,13 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
                         MATCH (p:Proceeding {key: line.crossref})
                         CREATE (i:Inproceeding {ID: toInteger(line.id), key: line.key, mdate: date(line.mdate), 
                         title: line.title, year: toInteger(line.year), booktitle: line.booktitle}) -[:PUBLISHED_IN]->(p)
+                        WITH line, i
+                        UNWIND split(line.author, '|') AS authors
+                        MERGE (author:Author {author: authors})
+                        CREATE (i)-[:AUTHORED_BY]->(author)
+                        WITH author, i, line
+                        WHERE author.author = split(line.author, '|')[0]
+                        CREATE (i)-[:CORRESPONDING_AUTHOR]->(author)
                     }
                     IN TRANSACTIONS
                     """);
@@ -143,11 +118,9 @@ public class PartA2_ZivkovicIvanovic implements AutoCloseable {
         try (var loader = new PartA2_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
             loader.createConstraints();
             loader.createIndexes();
-            loader.loadAuthors();
             loader.loadArticles();
             loader.loadProceedings();
             loader.loadInproceedings();
-            loader.loadAuthoredBy();
         }
     }
 }
