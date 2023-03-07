a0b08c8e14e5a19d19c427d91b4c925bc2eba8c9
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-03-07 22:42:16 +0100

    Query 2

--- src/main/java/org/example/PartB1_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartB1_ZivkovicIvanovic.java
@@ -1,8 +1,7 @@
 package org.example;
 
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.*;
+import org.neo4j.driver.Record;
 
 public class PartB1_ZivkovicIvanovic implements AutoCloseable {
 
@@ -17,9 +16,28 @@ public class PartB1_ZivkovicIvanovic implements AutoCloseable {
         driver.close();
     }
 
+    public void executeQuery2() {
+        System.out.println("Executing query 2...");
+        try (Session session = driver.session())
+        {
+            Result result = session.run("""
+                    MATCH (c:Conference)<-[:PART_OF]-(p:Proceeding)<-[:PUBLISHED_IN]-(:Inproceeding)-[:AUTHORED_BY]-(a:Author)
+                    WITH c.conference AS conference, a.author AS author, COUNT(DISTINCT p) AS cnt
+                    WHERE cnt > 3
+                    WITH conference, author, cnt
+                    RETURN conference, COLLECT(author) AS community
+                    """);
+            while (result.hasNext())
+            {
+                Record record = result.next();
+                System.out.println("conference: " +  record.get("conference").asString() + ", community: " + record.get("community").asList());
+            }
+        }
+    }
+
     public static void main(String... args) {
         try(var loader = new PartB1_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
-
+            loader.executeQuery2();
         }
     }
 }
