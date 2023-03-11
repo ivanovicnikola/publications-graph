be380f25d5f98162217320925f20dc9ba9caa513
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-03-11 18:18:03 +0100

    Find page rank

--- src/main/java/org/example/PartC_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartC_ZivkovicIvanovic.java
@@ -3,6 +3,8 @@ package org.example;
 import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Result;
 
 public class PartC_ZivkovicIvanovic implements AutoCloseable{
 
@@ -45,10 +47,53 @@ public class PartC_ZivkovicIvanovic implements AutoCloseable{
         }
     }
 
+    public void createCommunitySubgraph() {
+        System.out.println("Creating community subgraph...");
+        try (var session = driver.session()) {
+            session.run("""
+                    MATCH p=(n1)-[:CITES]->(n2)
+                    WHERE (EXISTS {
+                        MATCH (:Community{community:"database"})<-[:BELONGS_TO]-(:Journal)<-[:PUBLISHED_IN]-(n1)
+                    } OR EXISTS {
+                        MATCH (:Community{community:"database"})<-[:BELONGS_TO]-(:Conference)<-[:PART_OF]-(:Proceeding)<-[:PUBLISHED_IN]-(n1)
+                    })
+                    AND
+                    (EXISTS {
+                        MATCH (:Community{community:"database"})<-[:BELONGS_TO]-(:Journal)<-[:PUBLISHED_IN]-(n2)
+                    } OR EXISTS {
+                        MATCH (:Community{community:"database"})<-[:BELONGS_TO]-(:Conference)<-[:PART_OF]-(:Proceeding)<-[:PUBLISHED_IN]-(n2)
+                    })
+                    RETURN gds.alpha.graph.project('papers', n1, n2)
+                    """);
+        }
+    }
+
+    public void callPageRank() {
+        System.out.println("Performing page rank...");
+        try (var session = driver.session()) {
+            Result result = session.run("""
+                    CALL gds.pageRank.stream(
+                        'papers'
+                    )
+                    YIELD nodeId, score
+                    RETURN gds.util.asNode(nodeId).key AS key, score
+                    ORDER BY score DESC, key ASC\s
+                    LIMIT 100
+                    """);
+            while (result.hasNext())
+            {
+                Record record = result.next();
+                System.out.println("key: " +  record.get("key").asString() + ", score: " + record.get("score").asDouble());
+            }
+        }
+    }
+
     public static void main(String... args) {
         try(var loader = new PartC_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
             loader.findJournalCommunity();
             loader.findConferenceCommunity();
+            loader.createCommunitySubgraph();
+            loader.callPageRank();
         }
     }
 }
