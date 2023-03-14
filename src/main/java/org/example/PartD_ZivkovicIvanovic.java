67cc38d56ee1090276d86ffc85acad2f9c154acc
Nikola Ivanović <nikola.iv.99@gmail.com>
2023-03-14 20:15:26 +0100

    Node similarity

--- src/main/java/org/example/PartD_ZivkovicIvanovic.java
+++ src/main/java/org/example/PartD_ZivkovicIvanovic.java
@@ -59,11 +59,88 @@ public class PartD_ZivkovicIvanovic implements AutoCloseable {
         }
     }
 
+    public void connectAuthorsToJournals() {
+        System.out.println("Connecting authors to journals...");
+        try (var session = driver.session()) {
+            session.run("""
+                    MATCH p=(a:Author)<-[:AUTHORED_BY]-(:Article)-[:PUBLISHED_IN]->(j:Journal)
+                    MERGE (a)-[:WRITES_FOR]->(j)
+                    """);
+        }
+    }
+
+    public void connectAuthorsToConferences() {
+        System.out.println("Connecting authors to conferences...");
+        try (var session = driver.session()) {
+            session.run("""
+                    MATCH p=(a:Author)<-[:AUTHORED_BY]-(:Inproceeding)-[:PUBLISHED_IN]->(:Proceeding)-[:PART_OF]->(c:Conference)
+                    MERGE (a)-[:WRITES_FOR]->(c)
+                    """);
+        }
+    }
+
+    public void createWritesForSubgraph() {
+        System.out.println("Creating subgraph...");
+        try (var session = driver.session()) {
+            session.run("""
+                    MATCH (a:Author)-[:WRITES_FOR]->(p:Journal|Conference)
+                    RETURN gds.alpha.graph.project('writesFor', a, p)
+                    """);
+        }
+    }
+
+    public void callNodeSimilarity() {
+        System.out.println("Finding similar authors...");
+        try (var session = driver.session()) {
+            Result result = session.run("""
+                    CALL gds.nodeSimilarity.stream('writesFor')
+                    YIELD node1, node2, similarity
+                    RETURN gds.util.asNode(node1).author AS Author1, gds.util.asNode(node2).author AS Author2, similarity
+                    ORDER BY similarity DESCENDING, Author1, Author2
+                    """);
+            while (result.hasNext())
+            {
+                Record record = result.next();
+                System.out.println("Author1: " +  record.get("Author1").asString() + ", Author2: " + record.get("Author2").asString() + ", similarity: " + record.get("similarity").asDouble());
+            }
+        }
+    }
+
+    public void dropWritesForSubgraph() {
+        System.out.println("Dropping subgraph...");
+        try (var session = driver.session()) {
+            session.run("""
+                    CALL gds.graph.drop('writesFor')
+                    """);
+        }
+    }
+
+    public void deleteWritesForRelationships() {
+        System.out.println("Dropping writes for relationships...");
+        try (var session = driver.session()) {
+            session.run("""
+                    MATCH ()-[r:WRITES_FOR]->() DELETE r
+                    """);
+        }
+    }
+
     public static void main(String... args) {
+        //Run only if C is not ran beforehand
+        /*try(var loader = new PartC_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
+            loader.findJournalCommunity();
+        }*/
         try(var loader = new PartD_ZivkovicIvanovic("bolt://localhost:7687", "", "")) {
+            //betweenness
             loader.createSubgraph();
             loader.callBetweenness();
             loader.dropSubgraph();
+            //node similarity
+            loader.connectAuthorsToJournals();
+            loader.connectAuthorsToConferences();
+            loader.createWritesForSubgraph();
+            loader.callNodeSimilarity();
+            loader.dropWritesForSubgraph();
+            loader.deleteWritesForRelationships();
         }
     }
 }
